7c5c6bc9d4f5fc5bb890f3b2b70796d106af88e0
hlambson <54604304+hlambson@users.noreply.github.com>
2020-02-24 09:27:31 -0800

    Update avl.h

--- avl.h
+++ avl.h
@@ -4,29 +4,47 @@
 #include <stdexcept>
 #include <vector>
 #include "avlnode.h"
-
+//custom AVL Tree class written for Data Structures
+//used in a search engine program to store documents
 
 using namespace std;
 
 template <typename T>
 class AVL{
     public:
-
+        //default constructor
+        AVL(){
+            root=nullptr;
+        }
+        
+        //root node and getter for root
         AVLNode<T> *root;
         AVLNode<T>*& getRoot() {
             return root;
         }
-        AVL(){
-            root=nullptr;
-        }
-
+      
+    
     AVL(const AVL &rhs):
         root(nullptr){
         *this =rhs;
     }
-
+    
+    //destructor and make empty function
     ~AVL(){makeEmpty();}
-
+    
+    void makeEmpty(){
+        makeEmpty(root);
+    }
+    
+    void makeEmpty(AVLNode<T>* node) {
+        if (node != nullptr) { //recurse through the tree and delete every node
+            makeEmpty(node->left);
+            makeEmpty(node->right);
+            delete node;
+        }
+    }
+    
+    //check if the tree contains a specific node
     bool contains(T x, AVLNode<T>* node) {
         if (node != nullptr) {
             if (x < root->getElement()) {
@@ -43,20 +61,12 @@ class AVL{
             return false;
         }
     }
-
-    const int & findMin() const{
-        if(isEmpty())
-        throw underflow_error("underflow");
-        return findMin(root)->element;
+    
+    //return a specific node in the tree
+     AVLNode<T>* search(T& x) {
+        search(x,this->root);
     }
-
-    const int & findMax() const
-    {
-        if(isEmpty())
-            throw underflow_error("underflow");
-        return findMax(root)->element;
-    }
-
+    
     AVLNode<T>* search(T& x,AVLNode<T>* node){
         if (node != nullptr) {
             if (x < node->getElement()) {
@@ -74,46 +84,25 @@ class AVL{
         }
 
     }
-
-    AVLNode<T>* search(T& x) {
-        search(x,this->root);
-    }
     
-
+    //check if the tree is empty
     bool isEmpty() const{
         return root ==  nullptr;
     }
-
-    void makeEmpty(){
-        makeEmpty(root);
-    }
-
-    void makeEmpty(AVLNode<T>* node) {
-        if (node != nullptr) {
-            makeEmpty(node->left);
-            makeEmpty(node->right);
-            delete node;
-        }
-    }
-    void insert( T  x){
-        /*if (this == nullptr) {
-            AVL<T>* t = new AVL<T>();
-            insert(x, t->root);
-        }
-        else {*/
-        insert(x, root);
-       // }
-    }
-
+    
+    //return the height of the tree
     int height(AVLNode<T> *t) const{
         return t == nullptr? -1 : t ->height;
     }
+    
+    //return the maximum height
     int max (int lhs, int rhs)  const{
         return lhs > rhs ? lhs:rhs;
     }
-
-
-
+    
+    //rotation functions for balacing the tree
+    
+    //single rotate left
     void rotateWithLeftChild(AVLNode<T>* &k2) {
         AVLNode<T>* k1 = k2->left;
         k2->left = k1->right;
@@ -122,13 +111,15 @@ class AVL{
         k1->height = max(height(k1->left), (k2->height)) + 1;
         k2 = k1;
     }
-
+    
+    //double rotate left
     void doubleWithleftChild(AVLNode<T> * & k3){
 
         rotateWithRightchild(k3->left);
         rotateWithLeftChild(k3);
     }
-
+    
+    //single rotate right
     void rotateWithRightchild(AVLNode<T>* &k2) {
         AVLNode<T>* k1 = k2->right;
         k2->right  = k1->left;
@@ -138,12 +129,19 @@ class AVL{
         k2 = k1;
 
     }
-
+    
+    //double rotate right
     void doubleWithRightChild(AVLNode<T> * k3) {
         rotateWithLeftChild(k3->right);
         rotateWithRightchild(k3);
     }
 
+    
+    //insert into the tree 
+    void insert( T  x){
+        insert(x, root);
+    }
+    
     void insert(T &x, AVLNode<T>* &t) {
         if (t==nullptr) {
             t = new AVLNode<T>(x,NULL,NULL);
@@ -163,7 +161,6 @@ class AVL{
         }
         else if (t->element < x) {
             insert(x,t->right);
-            //cout << ((height(t->right) - (height(t->left))))<<endl;
             if ((height(t->right)) - (height(t->left)) > 1) {
                 if (x > t->right->element) {
                     rotateWithRightchild(t);
